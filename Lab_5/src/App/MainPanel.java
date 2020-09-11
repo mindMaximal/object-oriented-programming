f52caf8f1db310b32dd4de1808306d6090651f22
Евгений Волков <vamper.2012@gmail.com>
2020-09-11 19:18:22 +0800

    main

--- Lab_5/src/App/MainPanel.java
+++ Lab_5/src/App/MainPanel.java
@@ -19,33 +19,30 @@ public class MainPanel extends JPanel {
         listBtn.setForeground(Color.WHITE);
         listBtn.setBackground(Color.BLACK);
         listBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
-        listBtn.setBorder(BorderFactory.createEmptyBorder(50, 10, 50, 10));
-        ActionListener listBtnActionListener = new listBtnActionListener();
-        listBtn.addActionListener(listBtnActionListener);
+        listBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setBackground(Color.RED);
+            }
+        });
 
         addBtn.setBorderPainted(false);
         addBtn.setFocusPainted(false);
         addBtn.setForeground(Color.WHITE);
         addBtn.setBackground(Color.BLACK);
         addBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
-      /*  ActionListener addBtnActionListener = new addBtnActionListener();
-        addBtn.addActionListener(addBtnActionListener);*/
 
         removeBtn.setBorderPainted(false);
         removeBtn.setFocusPainted(false);
         removeBtn.setForeground(Color.WHITE);
         removeBtn.setBackground(Color.BLACK);
         removeBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
-      /*  ActionListener removetBtnActionListener = new removetBtnActionListener();
-        removetBtn.addActionListener(removetBtnActionListener);*/
 
         findBtn.setBorderPainted(false);
         findBtn.setFocusPainted(false);
         findBtn.setForeground(Color.WHITE);
         findBtn.setBackground(Color.BLACK);
         findBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
-        /*ActionListener findBtnActionListener = new findBtnActionListener();
-        findBtn.addActionListener(findBtnActionListener);*/
 
         setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
         add(addBtn);
@@ -54,11 +51,4 @@ public class MainPanel extends JPanel {
         add(findBtn);
     }
 
-    public class listBtnActionListener implements ActionListener {
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            setBackground(Color.RED);
-        }
-    }
 }
